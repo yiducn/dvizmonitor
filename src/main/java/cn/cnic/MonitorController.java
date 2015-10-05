4ace2c54f38ff5365eedaf24668a9cb0748c2792
yi du mac <duyi001@gmail.com>
2015-10-05 13:16:11 -0400

    add monitor of sdc

--- src/main/java/cn/cnic/MonitorController.java
+++ src/main/java/cn/cnic/MonitorController.java
@@ -1,18 +1,28 @@
 package cn.cnic;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.sql.*;
 
 @Controller
 public class MonitorController {
-    private static final String IP = "TODO"+":3306";
-    private static final String CONN_STRING = "jdbc:mysql://"+IP+"/platform2?\" +\n" +
-            "                            \"user=root&password=root";
+    private static final String IP = "TODO205"+":3306";
+    private static final String CONN_STRING = "jdbc:mysql://"+IP+"/platform2?" +
+            "user=root&password=root";
 //    private static final String CONN_STRING = "jdbc:mysql://localhost:3307/dviz?" +
 //                                          "user=root&password=";
     @RequestMapping("dvizdata.do")
@@ -28,8 +38,6 @@ public class MonitorController {
             // handle the error
         }
         try {
-                    DriverManager.getConnection("jdbc:mysql://localhost:3307/dviz?" +
-                            "user=root&password=");
             conn =
                     DriverManager.getConnection(CONN_STRING);
             Statement stmt = null;
@@ -70,4 +78,61 @@ public class MonitorController {
 
     }
 
+    /**
+     *
+     * @return monitor data of provinces
+     */
+    @RequestMapping("provdata.do")
+    public @ResponseBody String getData(){
+        try {
+            CloseableHttpClient httpclient = HttpClients.createDefault();
+            HttpGet httpGet = new HttpGet("http://www2.csdb.cn/getServiceInfo.jsp?serviceName=cloud");
+            CloseableHttpResponse response1 = httpclient.execute(httpGet);
+            try {
+                HttpEntity entity1 = response1.getEntity();
+                String value = getStringFromInputStream(entity1.getContent());
+                // do something useful with the response body
+                // and ensure it is fully consumed
+                System.out.println(value);
+                EntityUtils.consume(entity1);
+                return value;
+            } finally {
+                response1.close();
+            }
+        }catch(Exception e ){
+            e.printStackTrace();
+        }
+        return "";
+
+    }
+
+    // convert InputStream to String
+    private static String getStringFromInputStream(InputStream is) {
+
+        BufferedReader br = null;
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        try {
+
+            br = new BufferedReader(new InputStreamReader(is));
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return sb.toString();
+
+    }
 }
